58101eec99414c3df87e8e6617ed17cff55c0fd3
dfrancese <davefrancese@gmail.com>
2017-10-31 11:25:42 -0600

    setup index page with header

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const methodOverride = require('method-override');
+
+const routes = require('./routes/index')
+
+const app = express();
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.use(methodOverride('_method'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+})
